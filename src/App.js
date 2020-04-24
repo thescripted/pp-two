54f713945a71704e72aadf01b2cf0ca92dce0508
emanuallan <allanserna@gatech.edu>
2020-04-24 00:06:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Postform />
+          <Posts />
+        </header>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
